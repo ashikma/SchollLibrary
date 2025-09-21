098910556bc40cb371e42a92f364f64997fb2fc3
Alex <Alex@660084-COMPN-01>
2025-09-21 11:23:37 +0300

    Add project files.

---
+++ Models/Attributes/FirLeterCapitalAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryModels
+{
+    public class FirstLetterCapitalAttribute: ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            string str = value.ToString();
+            char firstLetter = str[0];
+            if(firstLetter<=65 || firstLetter >= 90)
+                return false;
+            for(int i = 1; i < str.Length; i++)
+            {
+                if (str[i] < 141 && str[i] > 172)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Models/Models/Author.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModels
+{
+    public class Author
+    {
+        string authorId;
+        string authorFirstName;
+        string authorLastName;
+        string authorYear;
+        int countryId;
+        string authorPicture;
+
+        public string AuthorId
+        {
+            get { return authorId; }
+            set { authorId = value; }
+        }
+
+        [Required(ErrorMessage = "You must enter Autor Last Name")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Last Name cannot be longer than 15 characters and less than 2")]
+        [FirstLetterCapital(ErrorMessage = "First letter must be capital")]
+        public string AuthorFirstName
+        {
+            get { return authorFirstName; }
+            set { authorFirstName = value; }
+        }
+        public string AuthorLastName
+        {
+            get { return authorLastName; }
+            set { authorLastName = value; }
+        }
+
+        [Required(ErrorMessage = "You must enter Autor Year")]
+        public string AuthorYear
+        {
+            get { return authorYear; }
+            set { authorYear = value; }
+        }
+        [Required(ErrorMessage = "You must enter Autor Country")]
+        public int CountryId
+        {
+            get { return countryId; }
+            set { countryId = value; }
+        }
+        [Required(ErrorMessage = "You must enter Autor Photo")]
+        public string AuthorPicture
+        {
+            get { return authorPicture; }
+            set { authorPicture = value; }
+        }
+
+    }
+    
+}
